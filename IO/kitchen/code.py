6bb661bb77cb029737cb52e5b71c01b24b53270c
lurbano <lurbano@earthsciweb.org>
2023-12-25 23:57:44 -0600

    fixes

--- IO/kitchen/code.py
+++ IO/kitchen/code.py
@@ -29,6 +29,7 @@ print("Start touch", touch.value)
 
 # brightness Knob
 brightKnob = uKnob(board.A2)
+l_lightsON = True
 
 solidColor = (255, 255, 255)
 modeColors = {}
@@ -137,7 +138,16 @@ def touchCheck():
         return True
     else:
         return False
- 
+    
+def setBrightness():
+    brightness = brightKnob.getPercent()/100
+    if brightness < 0.02:
+        ledPix.off()
+        l_lightsON = False
+    else:
+        l_lightsON = True
+        ledPix.brightness = brightness
+
 print(f"Listening on http://{wifi.radio.ipv4_address}:80")
 # Start the server.
 server.start(str(wifi.radio.ipv4_address))
@@ -158,11 +168,12 @@ while True:
                 # rainbow
                 for j in range(255):
                     for i in range(ledPix.nPix):
+                        setBrightness()
                         pixel_index = (i * 256 // ledPix.nPix) + j
                         
                         ledPix.pixels[i] = ledPix.wheel(pixel_index & 255, 0.5) 
-
-                    ledPix.pixels.show()
+                    if l_lightsON:
+                        ledPix.pixels.show()
                     server.poll()
                     if ledMode == "rainbow":
                         time.sleep(0.01)
@@ -205,6 +216,7 @@ while True:
         continue
 
         
+
 
 
 
